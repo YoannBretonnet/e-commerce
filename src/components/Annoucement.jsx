e527847a6aa3342a706ce568470b12f13da51123
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2023-03-14 12:19:02 +0100

    feat: annoucement component

---
+++ src/components/Annoucement.jsx
@@ -0,0 +1,15 @@
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+`
+
+function Annoucement() {
+    return (
+        <Container>
+            Super deal! Free shipping
+        </Container>
+    );
+}
+
+export default Annoucement;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import { Announcement } from "@material-ui/icons";
 import Navbar from "../../components/Navbar";
 
 function Home() {
     return (
         <div>
+            <Announcement/>
             <Navbar />
         </div>
     );
