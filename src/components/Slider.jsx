175f22960ac3803821393d2ff65163e688e1f1d5
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2023-03-14 19:53:26 +0100

    style: add background images on slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,14 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import { useState } from "react";
 import styled from "styled-components";
+import { sliderItems } from "../data";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   position: relative;
-  overflow: hiden;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -36,6 +38,8 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
+  transition: all 1.5s;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
@@ -47,13 +51,14 @@ const Slide = styled.div`
 
 const ImgContainer = styled.div`
   background-image: url(${(props) => props.img});
-  background-size: cover;
+  background-size: cover; 
   height: 100%;
   flex: 1;
   position: relative;
 `;
 const InfoContainer = styled.div`
   flex: 1;
+  width: 25%;
   padding: 50px;
   position: absolute;
   top: 0;
@@ -84,53 +89,32 @@ const Button = styled.button`
 `;
 
 function Slider() {
-  const handleClick = (direction) => {};
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction==="left"){
+      setSlideIndex(slideIndex > 0 ? slideIndex     -1 : 2)
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex +1 : 0)
+    }
+  };
 
   return (
     <Container>
       <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>
-      <Wrapper>
-        <Slide>
-          <ImgContainer
-            img={
-              "https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80"
-            }
-          >
-            <InfoContainer>
-              <Title>SUMMER SALES</Title>
-              <Desc>New beautiful arrivals at 50% off!</Desc>
-              <Button>SHOP NOW</Button>
-            </InfoContainer>
-          </ImgContainer>
-        </Slide>
-        <Slide>
-          <ImgContainer
-            img={
-              "https://images.unsplash.com/photo-1490945284861-4103094cc827?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80 "
-            }
-          >
-            <InfoContainer>
-              <Title>SUMMER SALES</Title>
-              <Desc>New beautiful arrivals at 50% off!</Desc>
-              <Button>SHOP NOW</Button>
-            </InfoContainer>
-          </ImgContainer>
-        </Slide>
-          <Slide>
-          <ImgContainer
-            img={
-              "https://images.pexels.com/photos/755023/pexels-photo-755023.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            }
-          >
-            <InfoContainer>
-              <Title>SUMMER SALES</Title>
-              <Desc>New beautiful arrivals at 50% off!</Desc>
-              <Button>SHOP NOW</Button>
-            </InfoContainer>
-          </ImgContainer>
-        </Slide>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide key={item.id}>
+            <ImgContainer img={item.img}>
+              <InfoContainer>
+                <Title>{item.title}</Title>
+                <Desc>{item.desc}</Desc>
+                <Button>SHOP NOW</Button>
+              </InfoContainer>
+            </ImgContainer>
+          </Slide>
+        ))}
       </Wrapper>
       <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightOutlined />
---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
+        title: "SUMMER SALES",
+        desc: "Don't compromise on style! Get flat 30% off for new arrivales"
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1490945284861-4103094cc827?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
+        title: "SUMMER SALES",
+        desc: "Don't compromise on style! Get flat 30% off for new arrivales"
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/755023/pexels-photo-755023.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "SUMMER SALES",
+        desc: "Don't compromise on style! Get flat 30% off for new arrivales"
+    }
+]
