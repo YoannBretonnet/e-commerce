ae70e467c13561bf73f132c0850933edad69215d
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2023-03-15 22:08:12 +0100

    feat: productList page with sorted options

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+
+import Annoucement from "../components/Annoucement";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import ProductsList from "../components/ProductsList";
+import Slider from "../components/Slider";
+
+
+function Home() {
+    return (
+        <div>
+            <Annoucement/>
+            <Navbar/>
+            <Slider/>
+            <Categories/>
+            <ProductsList/>
+            <Newsletter/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default Home;
