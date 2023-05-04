bfca13c78e95fa2da45ecfb76ee1752cbb4168c9
Yoann Bretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2023-05-04 10:29:37 +0200

    Revert "update with API"
    This reverts commit bcf1486bbacdf7623007a052a3cc435ad39d045b.

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://sangha-ecommerce-api.netlify.app/api/"
+const BASE_URL = "http://localhost:5000/api/"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MjQzYWVhYTJmMmZlZjVkY2RhODdkMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MDg3OTQ2NiwiZXhwIjoxNjgxMTM4NjY2fQ.kK4xPVH1EZY1CbVa8frDxqu9hNDM4XmR7Py7KjQDdvY";
 
 export const publicRequest = axios.create({
